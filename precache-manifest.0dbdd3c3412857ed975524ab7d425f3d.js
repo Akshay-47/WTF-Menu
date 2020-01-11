773ae67ac11e5cf828caea56f6a246eff93546cb
Akshay-47 <akshay.lokray47@gmail.com>
2020-01-11 22:44:25 +0530

    Updates

---
+++ precache-manifest.0dbdd3c3412857ed975524ab7d425f3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb0cb12ba13c13a2565fdc27033a1b6",
+    "url": "/WTF/index.html"
+  },
+  {
+    "revision": "1704eccfeb2c584801ab",
+    "url": "/WTF/static/css/main.0e67a8b0.chunk.css"
+  },
+  {
+    "revision": "f4d1aaaa84f2f4aa269c",
+    "url": "/WTF/static/js/2.134f1992.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/WTF/static/js/2.134f1992.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1704eccfeb2c584801ab",
+    "url": "/WTF/static/js/main.4108dc63.chunk.js"
+  },
+  {
+    "revision": "6a7092da852adeaa83f2",
+    "url": "/WTF/static/js/runtime-main.08e8ed6a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WTF-MENU/precache-manifest.ed4033b8c712af58663c7348208bf469.js"
+  "/WTF/precache-manifest.0dbdd3c3412857ed975524ab7d425f3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WTF-MENU/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WTF/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
